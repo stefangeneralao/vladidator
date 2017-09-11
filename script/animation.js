7475320eb2f5c50f435f1cc148add7c0404852f6
Christopherac <christopherac@gmail.com>
2017-09-11 20:34:44 +0200

    Extended GUI

--- script/animation.js
+++ script/animation.js
@@ -1,4 +1,18 @@
 $(function(){
-    alert("Hel");
-
+    $("#button").click(function(){
+        $("#input_content").fadeOut(500,function(){
+            var result = textAnalyzer();
+            
+            var wordCount = result.wordCount;
+            var charCount = result.characterCount;
+            var whiteSpaceCount = result.whiteSpaceCount;
+            var wordList = result.wordList;
+            
+            $("#result_list").slideToggle(500,function(){
+                $("#wordCount").text(wordCount);
+                $("#charCount").text(charCount);
+                $("#whiteSpaceCount").text(whiteSpaceCount);
+            });
+        });
+    });
 });
