49c0a44a8543884292666eeecba4532977355050
ChristopherAc <christopheranderssonc@gmail.com>
2017-09-11 23:05:56 +0200

    "minor design changes"

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # vladidator
 qurridutt was here
 stefangeneralao was here
+Christopherac was here...
--- script/animation.js
+++ script/animation.js
@@ -1,18 +1,32 @@
 $(function(){
-    $("#button").click(function(){
-        $("#input_content").fadeOut(500,function(){
-            var result = textAnalyzer();
-            
-            var wordCount = result.wordCount;
-            var charCount = result.characterCount;
-            var whiteSpaceCount = result.whiteSpaceCount;
-            var wordList = result.wordList;
-            
-            $("#result_list").slideToggle(500,function(){
-                $("#wordCount").text(wordCount);
-                $("#charCount").text(charCount);
-                $("#whiteSpaceCount").text(whiteSpaceCount);
-            });
-        });
+
+  $("#text").keydown(function(){
+
+    $("#text").keyup(function(){
+
+      value = $("#text").val();
+      console.log(value.length+1)
+      if(value.length > 0){
+        $("#button").fadeIn(500);
+      }
+      else if(value.length == 0){
+        $("#button").fadeOut(500)
+      }
+    })
+  })
+
+  $("#button").click(function(){
+    $("#input_content").fadeOut(500,function(){
+      var result = textAnalyzer();
+      var wordCount = result.wordCount;
+      var charCount = result.characterCount;
+      var whiteSpaceCount = result.whiteSpaceCount;
+      var wordList = result.wordList;
+      $("#wordCount").text(wordCount);
+      $("#charCount").text(charCount);
+      $("#whiteSpaceCount").text(whiteSpaceCount);
+      $("#result_list").slideToggle(500,function(){
+      });
     });
+  });
 });
